427410a46f05a15a0b2bc07d0333c8fce58f7664
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-10-04 20:20:10 +0200

    3. Mattetabell

---
+++ Mattetabell/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Mattetabell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int tal = 1; tal <= 10; tal++)
+            {
+                for (int multi =1; multi <= 10; multi++)
+                {
+                    Console.Write(tal * multi + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
