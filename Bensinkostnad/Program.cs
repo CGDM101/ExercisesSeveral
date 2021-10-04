80a709b2c74a6a3477138a93d83e51ed8f5f75fe
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-10-04 20:11:56 +0200

    1: Bensinkostnad

--- Bensinkostnad/Program.cs
+++ Bensinkostnad/Program.cs
@@ -6,7 +6,12 @@ namespace Bensinkostnad
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double distance = 344.24; // km
+            double fuelConsumption = 6.7; // l per 100 km
+            double fuelPrice = 17.24; // per l
+            double price = (fuelConsumption * 3.4424) * 17.24;
+
+            Console.WriteLine("Kostnad{0} ", price); // ~ 398
         }
     }
 }
