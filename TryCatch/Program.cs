16f8e04572cf8712370d4a85c41bcbc20c48ff00
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-11-02 19:32:05 +0100

    Null error.

--- TryCatch/Program.cs
+++ TryCatch/Program.cs
@@ -23,6 +23,38 @@ namespace TryCatch
                 Console.WriteLine(ex.Message);
                 //throw;
             }
+
+            // =============
+
+            Person person = null;
+            try
+            {
+                person.Name = "Jack Sparrow";
+            }
+            // Triggas av null error:
+            catch (System.NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message); // Hantera vad som gått fel.
+            }
+            // Triggas av alla andra error:
+            catch(System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            // Ifall minnet behöver städas upp:
+            finally
+            {
+                person = null;
+            }
         }
     }
+
+    class Person 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    // Ha minimalt med kod i Try Catch, för att lätt identifiera vad som gått fel. Hellre flera Try Catch efter varandra.
+    // Try Catch är långsammare än if-satser.
 }
