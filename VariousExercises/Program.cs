738d2cf13707e768ae3063fdec0000590e076d11
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-11-03 18:34:44 +0100

    Added while and dowhile exercises.

--- VariousExercises/Program.cs
+++ VariousExercises/Program.cs
@@ -80,7 +80,74 @@ namespace VariousExercises
                 Console.WriteLine(item);
             }
 
-            // NÄSTLADE LOOPAR:
+            // NÄSTLADE LOOPAR: =========================================== WIP
+
+
+            // MER WHILE:
+
+            // Gör en While loop som ska köra max 5 gånger. I den slumpar du fram tal mellan 1 och 100.
+            Console.WriteLine("WHILE:");
+
+            int counter = 0;
+            while (counter < 5) // Måste var fem?! Eller att counter börjar på 1.
+           {
+                counter++;
+                Random random = new Random();
+                int tal = random.Next(1, 101);
+                Console.WriteLine(tal);
+            }
+
+            // Gör en while loop som körs tills ett slumptal mellan 1 och 100 blir 55
+            Console.WriteLine("while igen:");
+
+            int räknare = 0; // Egentligen inte nödvändig för uppgiften.
+
+            int slumptal = 0;
+            while (slumptal != 55)
+            {
+                Random rng = new Random();
+                slumptal = rng.Next(1, 101);
+                Console.WriteLine(slumptal);
+
+                räknare++;
+            }
+            Console.WriteLine("Det tog " + räknare + " ggr att nå 55");
+
+
+            // MER DOWHILE:
+            Console.WriteLine("do while:"); // ======================================= WIP
+
+            bool check = true;
+            do // så länge check är sant.
+            {
+                check = !check; // ändra check till false.
+                Console.WriteLine(check); // skriva ut checks värde för varje runda.
+            } while (check);
+
+            // Skriv ett spel där datorn ska gissa ett tal mellan 1 och 1000 som du tänker på.
+            Console.WriteLine("gissa numret:");
+
+            int myNum = 101;
+            int computerGuess;
+            do
+            {
+                Random r = new Random();
+                computerGuess = r.Next(1, 1001);
+            } while (myNum != computerGuess);
+
+            //if (computerGuess < myNum)
+            //{
+            //    Console.WriteLine("För lågt! Gissa igen!");
+            //}
+            //else if (computerGuess > myNum)
+            //{
+            //    Console.WriteLine("För högt! Gissa igen!");
+            //}
+            //else if (computerGuess == myNum)
+            //{
+            //    Console.WriteLine("yes!!");
+            //}
+
         }
     }
 }
